48fc59412d828c82e1adafcf2517360dfead0043
Abdiel <abdiel2012p@gmail.com>
2024-05-15 17:58:39 -0600

    Diseño del componente Detalles de Peliculas terminado

--- src/app/components/detalle/detalle.component.ts
+++ src/app/components/detalle/detalle.component.ts
@@ -1,4 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { ModalController } from '@ionic/angular';
+import { Cast, PeliculaDetalle } from 'src/app/interfaces/interfaces';
 import { MoviesService } from 'src/app/services/movies.service';
 
 @Component({
@@ -9,24 +11,34 @@ import { MoviesService } from 'src/app/services/movies.service';
 export class DetalleComponent  implements OnInit {
 
   @Input() id!: number;
+  pelicula: PeliculaDetalle = {};
+  oculto = 150;
+  actores: Cast[] = [];
 
-  constructor(private movieService: MoviesService) { }
+  constructor(private movieService: MoviesService,
+              private modalCtrl:ModalController) { }
 
   ngOnInit() {
-    console.log('Datos');
     this.movieService.getPeliculaDetalle(this.id)
       .subscribe(
         resp => {
-          console.log(resp.title);
+          this.pelicula= resp;
         }
       );
 
       this.movieService.getActoresPelicula(this.id)
       .subscribe(
         resp => {
-          console.log(resp.crew);
+          this.actores = resp.cast;
         }
       );
+  }
+  regresar(){
+    this.modalCtrl.dismiss();
+  }
+
+  favorito(){
+
   }
 
 }
